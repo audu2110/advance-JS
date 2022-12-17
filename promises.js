900829c5fc455198e5b3f261dda1c95e4dc7db63
audu2110 <91955066+audu2110@users.noreply.github.com>
2022-12-17 09:42:59 +0530

    Update promises.js

--- promises.js
+++ promises.js
@@ -40,57 +40,67 @@ const posts=[
     {title:'Post One' , body:'This is post One'},
     {title:'Post Two' , body:'This is post TWo'}
 ];
-
-function getPosts(){
+function getPosts()
+{
     setTimeout(() => {
-        let output='';
-        posts.forEach((post,index) => {
-            output+=`<li>${post.title}</li>`
-        });
-        document.body.innerHTML=output;
+        let output=""
+        for(let i=0;i<posts.length;i++)
+        {
+            output+=`<li>${posts[i].title}</li>`
+        }
+        document.body.innerHTML=output
     }, 1000);
 }
 
-
-function createPost(post){
-    return new Promise((resolve,reject)=>{
+function createPost(post,post4)
+{
+    return new Promise((resolve,reject)=>
+    {
         setTimeout(() => {
-            posts.push(post);
-            const error=false;
-            if(!error){
-                resolve();
-            }
-            else{
-                reject('error: something went wrong')
-            }
+            posts.push(post,{...post4});
+
+         const error= false
+         if(!error){
+resolve()
+         }
+         else{reject("Error:something went wrong")}
+
         }, 2000);
-    })
-}
-
-
-function deletePost(){
+     });
+    }
+function removePost()
+{
     return new Promise((resolve,reject)=>{
         setTimeout(() => {
-            posts.pop();
-            const error=false;
-            if(!error){
-                resolve();
+
+            if(posts.length>0)
+            {
+resolve(posts.pop())
             }
-            else{
-                reject('error: something went wrong')
-            }
-        }, 3000);
+            else{reject("Array is empty now")}
+
+        }, 1000);
     })
-}
+      }
+createPost({title:"Post Three", body:"This is post Three"},{title:"Post Four", body:"This is post Four"})
 
+     .then(()=>{
+      getPosts()
+    removePost().then(()=>{
+       getPosts()
+        removePost().then(()=>{
+            getPosts()
+            removePost().then(()=>{
+                getPosts()
+                removePost().then(()=>{
+             getPosts()
+                removePost().then(()=>{})
 
-createPost({title:'Post Three',body:'This is post Three'}).then(getPosts);
-deletePost().then(getPosts);
-deletePost().then(getPosts);
-deletePost().then(getPosts);
-setTimeout(() => {
-    createPost({title:'Post Four',body:'This is post Four'}).then(getPosts);
-}, 4000);
-setTimeout(() => {
-    deletePost().then(getPosts);
-}, 5000);
+                .catch((err)=>{console.log("inside catch block",err)
+            })
+            })
+
+    })
+})
+})
+     }) 
