b5a0a707cd881f727581db4287b14b0cb6453ba4
audu2110 <91955066+audu2110@users.noreply.github.com>
2022-12-18 17:08:43 +0530

    Update async.js

--- async.js
+++ async.js
@@ -11,3 +11,59 @@ promisewifeBringingTicks.then((t)=>{
 });
 console.log('person4: shows ticket');
 console.log('person5: shows ticket');
+
+
+console.log('person1: shows ticket');
+console.log('person2: shows ticket');
+
+const promisewifeBringingTicks= new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve('ticket ');
+    }, 3000);
+});
+
+const getPopcorn= promisewifeBringingTicks.then((t)=>{
+    console.log('husband: we should go in');
+    console.log('wife: no i am hungry');
+    return new Promise((resolve,reject)=>resolve(`${t} popcorn`));
+});
+const getButter= promisewifeBringingTicks.then((t)=>{
+    console.log('husband: we should go in');
+    console.log('wife: no i need butter');
+    return new Promise((resolve,reject)=>resolve(`${t} popcorn`));
+});
+
+
+getButter.then((t)=>console.log(t));
+console.log('person4: shows ticket');
+console.log('person5: shows ticket');
+
+
+console.log('person1: shows ticket');
+console.log('person2: shows ticket');
+
+const promisewifeBringingTicks= new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve('ticket ');
+    }, 3000);
+});
+
+const getPopcorn= promisewifeBringingTicks.then((t)=>{
+    console.log('husband: we should go in');
+    console.log('wife: no i am hungry');
+    return new Promise((resolve,reject)=>resolve(`${t} popcorn`));
+});
+const getButter= promisewifeBringingTicks.then((t)=>{
+    console.log('husband: we should go in');
+    console.log('wife: no i need butter');
+    return new Promise((resolve,reject)=>resolve(`${t} butter`));
+});
+const getColdDrinks= promisewifeBringingTicks.then((t)=>{
+    console.log('husband: we should go in');
+    console.log('wife: no i need coldDrink');
+    return new Promise((resolve,reject)=>resolve(`${t} coldDrinks`));
+});
+
+getColdDrinks.then((t)=>console.log(t));
+console.log('person4: shows ticket');
+console.log('person5: shows ticket');
