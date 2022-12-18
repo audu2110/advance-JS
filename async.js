a762fc5830caa8ef3cb43f2c677f5ac086d989f7
audu2110 <91955066+audu2110@users.noreply.github.com>
2022-12-18 17:01:22 +0530

    Create async.js

---
+++ async.js
@@ -0,0 +1,13 @@
+console.log('person1: shows ticket');
+console.log('person2: shows ticket');
+
+const promisewifeBringingTicks= new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve('ticket ');
+    }, 3000);
+});
+promisewifeBringingTicks.then((t)=>{
+    console.log(`person3: shows ${t}`)
+});
+console.log('person4: shows ticket');
+console.log('person5: shows ticket');
